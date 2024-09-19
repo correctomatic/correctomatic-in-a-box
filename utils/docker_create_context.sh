58fadbd1b96e795471a5ad18a5eb626bf433be2d
Alvaro Maceda <alvaro@alvaromaceda.es>
2024-09-19 07:28:19 +0200

    Trying to improve the context creation script

--- utils/docker_create_context.sh
+++ utils/docker_create_context.sh
@@ -1,10 +1,54 @@
 #! /usr/bin/env bash
 
+# Check if the first parameter is provided
+if [ -z "$1" ]; then
+    echo "Error: No environment or file provided."
+    echo "Usage: $0 [PROD|DEV|/path/to/file.yml]"
+    exit 1
+fi
+
+# If we are in "utils" directory, we need to go one directory up
+current_dir=$(basename "$PWD")
+if [ "$current_dir" == "utils" ]; then
+    UP="../"
+fi
+
+PROD_CONFIG_FILE="${UP}inventories/prod/group_vars/all/config.yml"
+DEV_CONFIG_FILE="${UP}inventories/dev/group_vars/all/config.yml"
+DEFAULT_DOCKER_HOST="dev.registry.correctomatic.org"
+
+case "$1" in
+    PROD)
+        CONFIG_FILE="$PROD_CONFIG_FILE"
+        ENVIRONMENT="prod"
+        ;;
+    DEV)
+        CONFIG_FILE="$DEV_CONFIG_FILE"
+        ENVIRONMENT="dev"
+        ;;
+    *)
+        CONFIG_FILE="$1"
+        ENVIRONMENT="custom"
+        ;;
+esac
+
+# Check if yq is installed
+if command -v yq &> /dev/null; then
+    REMOTE_DOCKER_HOST=$(yq '.docker.domain' "$CONFIG_FILE")
+    # Check if yq succeeded or if registry.domain is empty
+    if [ $? -ne 0 ] || [ -z "$REMOTE_DOCKER_HOST" ]; then
+        echo "Error: Failed to read registry.domain from file with yq."
+        exit 1
+    fi
+else
+    echo "yq is not installed, using default value for docker host."
+    REMOTE_DOCKER_HOST=$DEFAULT_DOCKER_HOST
+fi
+
 # You MUST use the domain defined in the configuration file: docker.domain
 # The certificates are generated for that specific domain
-REMOTE_DOCKER_HOST=docker.correctomatic.alvaromaceda.es
-CONTEXT_NAME=correctomatic_vps
-CERTIFICATES_DIR="${HOME}/.correctomatic/certs"
+CONTEXT_NAME=correctomatic_$ENVIRONMENT
+CERTIFICATES_DIR="${HOME}/.correctomatic/"$ENVIRONMENT"/certs"
 
 CA_CERT_PATH=$CERTIFICATES_DIR/ca.pem
 CLIENT_CERT_PATH=$CERTIFICATES_DIR/cert.pem
